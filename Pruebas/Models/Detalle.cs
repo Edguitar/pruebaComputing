bc4d98c169b16e84758e3375b5691571d082ba7b
Edguitar <ases28090502@gmail.com>
2014-07-02 23:10:29 -0500

    Initial commit

---
+++ Pruebas/Models/Detalle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pruebas.Models
+{
+    public class Detalle
+    {
+        public int FacturaId { get; set; }
+
+        public int ProductoId { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public decimal Cantidad { get; set; }
+    }
+}
---
+++ Pruebas/Models/Factura.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pruebas.Models
+{
+    public class Factura
+    {
+        public int Id { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public string Ruc { get; set; }
+
+        public string ClienteNombre { get; set; }
+
+        public ICollection<Detalle> Detalle { get; set; }
+
+        public Factura()
+        {
+            this.Detalle = new HashSet<Detalle>();
+        }
+    }
+}
